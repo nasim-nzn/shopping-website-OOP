267aa3751df681b13864b454cb11178e3da3b74d
nasim <nasimniaziann@gmail.com>
2024-02-28 10:50:30 +0330

    Add event to cart buttons

--- models/Cart.js
+++ models/Cart.js
@@ -4,6 +4,7 @@ class Cart {
     this.price = price;
     this.products = [];
     this.toShow = [];
+    this.parent.addEventListener("click", this);
   }
 
   showProducts() {
@@ -64,6 +65,44 @@ class Cart {
     `;
 
     return controlJSX;
+  }
+
+  handleEvent(event) {
+    const tagName = event.target.tagName;
+    const id = event.target.dataset.id;
+    const type = event.target.innerText;
+
+    if (tagName !== "BUTTON") return;
+
+    switch (type) {
+      case "+":
+        this.increase(id);
+        break;
+      case "-":
+        this.decrease(id);
+        break;
+      case "remove":
+        this.remove(id);
+        break;
+    }
+  }
+
+  increase(id) {
+    const product = this.products.find((p) => p.id === +id);
+    this.products.push(product);
+    this.showProducts();
+  }
+
+  decrease(id) {
+    const index = this.products.findIndex((p) => p.id === +id);
+    this.products.splice(index, 1);
+    this.showProducts();
+  }
+
+  remove(id) {
+    const newProducts = this.products.filter((p) => p.id !== +id);
+    this.products = newProducts;
+    this.showProducts();
   }
 }
 
